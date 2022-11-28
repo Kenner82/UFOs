f7fbd40aaefbffcd391c5be417a9d8f848a33bfd
Kenner82 <kenner_82@hotmail.com>
2022-11-27 18:01:11 -0800

    Finish app.js code

--- static/js/app.js
+++ static/js/app.js
@@ -36,6 +36,34 @@ function buildTable(data) {
           }
         );
       });
-
-
 }
+
+function handleClick() { 
+    // telling D3 to look for the first #datetime id in the HTML tags
+    // By chaining .property("value"); to the d3.select function, we're telling D3 not only to look for where our date values are stored on the webpage, but to actually grab that information and hold it in the "date" variable.
+    let date = d3.select("#datetime").property("value");
+
+    // default filter will actually be the original table data because we want users to refine their search on their own terms
+    let filteredData = tableData;
+
+
+    // Check to see if a date was entered and filter the data using that date.
+    if (date) {
+        // Apply `filter` to the table data to only keep the rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+
+    // Rebuild the table using the filtered data. If no date was entered, then filteredData will just be the original tableData.
+    buildTable(filteredData);
+    };
+}
+
+
+// Our selector string contains the id for another HTML tag. 
+// (We'll assign a unique id to a button element in the HTML called "filter-btn".) 
+// This time it'll be included in the button tags we create for our filter button. 
+// By adding this, we're linking our code directly to the filter button. 
+// Also, by adding .on("click", handleClick);, we're telling D3 to execute our handleClick() function when the button with an id of filter-btn is clicked
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
